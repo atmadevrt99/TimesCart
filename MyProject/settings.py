5fec523684b1e52cc2e7b0f3cc4b1d74f23a3cc0
atmadevrt99 <atmadevrt99@gmail.com>
2022-03-01 14:48:56 +0530

    first commit

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,10 +123,11 @@ USE_TZ = True
 #STATICFILES_DIRS=[STATIC_DIR]
 
 
-
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
